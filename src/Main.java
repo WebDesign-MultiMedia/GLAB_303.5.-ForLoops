eb2e71ab4ba7663214bdf3e09e695dcb2c6471df
Julio <youremail@example.com>
2024-07-08 00:19:35 -0400

    GLAB 303.5.1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Example 1
+        int n = 5;
+
+        for (int i = 1; i <= n; ++i){
+            System.out.println("Welcome to the Bronx");
+        }
+
+
+    }
+
+
+}
