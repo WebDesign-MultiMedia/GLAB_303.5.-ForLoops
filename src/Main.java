f61320bbd59b923bd2592a383c3e59bcdcab5ee4
Julio <youremail@example.com>
2024-07-08 00:40:56 -0400

    Ex 4 completed

--- src/Main.java
+++ src/Main.java
@@ -47,6 +47,12 @@ public class Main {
 
             //Example 4
 
+        for (int x=1; x<=5; x++){
+            for (int y =1; y <= x; y++){
+                System.out.print("$ ");
+            }
+            System.out.println();
+ }
 
     }
 }
